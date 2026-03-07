8902b94dd3b6747db0c72cffbc8d1188d141da1d
Adam Holisky <adam.holisky@gmail.com>
2026-03-07 20:45:08 +0000

    update

--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,12 +1,21 @@
 var indexSectionsWithContent =
 {
+  0: "abv",
+  1: "ab",
+  2: "v"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "files",
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Files",
+  2: "Functions"
 };
 
